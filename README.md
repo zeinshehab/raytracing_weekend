661dcd951be8b4592512adae31c11b156f18b5e1
zeinshehab <zeinshehab@outlook.com>
2025-11-28 17:33:39 +0200

    Implemented base ray tracer

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ray Tracing in One Weekend
+
+- `build.sh` will compile and run then use `feh` to display the image
+- I used `feh` to display the `ppm` image but it can be any software.
---
+++ camera.h
@@ -0,0 +1,33 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "ray.h"
+
+class camera {
+    public:
+        camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float aperture, float focus_dist) { // vfov is top to bottom in degrees
+            lens_radius = aperture / 2;
+            float theta = vfov * M_PI / 180;
+            float half_height = tan(theta/2);
+            float half_width = aspect * half_height;
+            origin = lookfrom;
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
+            lower_left_corner = origin - half_width*focus_dist*u - half_height*focus_dist*v - focus_dist*w;
+            horizontal = 2*half_width*focus_dist*u;
+            vertical = 2*half_height*focus_dist*v;
+        }
+        ray get_ray(float u, float v) {
+            return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+        }
+
+        vec3 lower_left_corner;
+        vec3 horizontal;
+        vec3 vertical;
+        vec3 origin;
+        vec3 u, v, w;
+        float lens_radius;
+};
+
+#endif // CAMERA_H
